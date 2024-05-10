96a87ba9b1196634365efe19aa29cd8189289ed7
Simon S <107688168+jit-compiler@users.noreply.github.com>
2024-05-10 15:20:19 +0200

    changes to joystick and ino

--- driverbot-site/src/lib/components/Slider.svelte
+++ driverbot-site/src/lib/components/Slider.svelte
@@ -1,81 +1,85 @@
 <script lang="ts">
-    export let setSettings : Function;
-    export let setting : string
-    export let checked = false;
+  export let setSettings: Function;
+  export let setting: string;
+  export let checked = false;
 </script>
 
 <label class="switch">
-<input type="checkbox" checked={checked} on:change={(e) => {
-    if (e.target) {
+  <input
+    type="checkbox"
+    {checked}
+    on:change={(e) => {
+      if (e.target) {
         // @ts-ignore
         setSettings(setting, e.target.checked);
-    }
-  }}/>
+      }
+    }}
+  />
   <span class="slider round"></span>
 </label>
 
 <style>
-    /* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 52px;
-  height: 24px;
-  margin: 8px;
-}
+  /* The switch - the box around the slider */
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 52px;
+    height: 24px;
+    margin: 8px;
+  }
 
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
+  /* Hide default HTML checkbox */
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #111;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #111;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 17px;
-  width: 17px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 17px;
+    width: 17px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-input:checked + .slider {
-  background-color: #7289D9;
-}
+  input:checked + .slider {
+    background-color: #7289d9;
+  }
 
-input:focus + .slider {
-  box-shadow: 0 0 1px #7289D9;
-}
+  input:focus + .slider {
+    box-shadow: 0 0 1px #7289d9;
+  }
 
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
 
-.slider.round:before {
-  border-radius: 50%;
-}
+  .slider.round:before {
+    border-radius: 50%;
+  }
 </style>
--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -1,22 +1,64 @@
-#define motorPinRightDir  0//D2
-#define motorPinRightSpeed 5//D1
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* ssid = "ABB_Gym_IOT";
+const char* password = "Welcome2abb";
+const char* mqtt_server = "10.22.3.254";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(motorPinRightDir, OUTPUT);
-  pinMode(motorPinRightSpeed, OUTPUT);
-
   Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  int speed = 1024;
-  int dir = 0;
-
-  delay(2200);
-  digitalWrite(motorPinRightDir, dir);
-  analogWrite(motorPinRightSpeed, speed);
-      delay(2200);      
-  digitalWrite(motorPinRightDir, 1);
-  analogWrite(motorPinRightSpeed, speed);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 }
