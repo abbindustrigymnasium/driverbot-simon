057d8c407e81144982190d2e15a6bf31940a9bd9
Simon S <107688168+jit-compiler@users.noreply.github.com>
2024-05-20 13:30:53 +0200

    updated motortest

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -1,27 +1,44 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Servo.h>
 
-// Update these with values suitable for your network.
-const char* ssid = "ABB_Gym_IOT";
-const char* password = "Welcome2abb";
-const char* mqtt_server = "10.22.3.254";
+#define motorPinRightDir 0   // D2
+#define motorPinRightSpeed 5 // D1
+#define servoPin 2           // D4
+
+const char *ssid = "ABB_Gym_IOT";
+const char *password = "Welcome2abb";
+const char *mqtt_server = "35.157.170.208"; // test.mosquitto.org
+const String directionTopic = "simon.svoboda@hitachigymnasiet.se/direction";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+Servo servo;
 
-void setup_wifi() {
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi()
+{
+
   delay(10);
-  // Connect to WiFi network
+  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
+
+  randomSeed(micros());
 
   Serial.println("");
   Serial.println("WiFi connected");
@@ -29,18 +46,38 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() {
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String message;
+  for (int i = 0; i < length; i++)
+  {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+  determineDirection(message);
+}
+
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
+      client.subscribe(directionTopic.c_str());
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -50,15 +87,109 @@ void reconnect() {
   }
 }
 
-void setup() {
+void setup()
+{
+  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(motorPinRightDir, OUTPUT);
+  pinMode(motorPinRightSpeed, OUTPUT);
+  servo.attach(servoPin);
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
+}
+
+void determineDirection(String message)
+{
+  if (message == "n")
+  {
+    forward();
+  }
+  else if (message == "s")
+  {
+    backwards();
+  }
+  else if (message == "w")
+  {
+    turnLeft();
+  }
+  else if (message == "e")
+  {
+    turnRight();
+  }
+  else if (message == "ne")
+  {
+    forward();
+    turnRight();
+  }
+  else if (message == "nw")
+  {
+    forward();
+    turnLeft();
+  }
+  else if (message == "se")
+  {
+    backwards();
+    turnRight();
+  }
+  else if (message == "sw")
+  {
+    backwards();
+    turnLeft();
+  }
+  else if (message == "stop")
+  {
+    stop();
+  }
+  else
+  {
+    Serial.println("Invalid direction");
+  }
+}
+
+void forward()
+{
+  digitalWrite(motorPinRightDir, HIGH);
+  analogWrite(motorPinRightSpeed, 255); // Speed motor
+  servo.write(90); // set servo to mid-point
+  Serial.println("Forward");
+}
+
+void backwards()
+{
+  digitalWrite(motorPinRightDir, LOW);
+  analogWrite(motorPinRightSpeed, 255); // Speed motor
+  servo.write(90); // set servo to mid-point
+  Serial.println("Backward");
+}
+
+void turnLeft()
+{
+  servo.write(0); // set servo to minimum
+  Serial.println("Left");
+}
+
+void turnRight()
+{
+  servo.write(180); // set servo to maximum
+  Serial.println("Right");
+}
+
+void stop()
+{
+  digitalWrite(motorPinRightDir, LOW);
+  analogWrite(motorPinRightSpeed, 0); // Stop motor
+  servo.write(90); // set servo to mid-point
+  Serial.println("Stop");
 }
